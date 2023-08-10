46c313a58d9d02bd98d9ab7ba1a1840edd5d8561
Ivan Cedeno <141590533+ivan-cedeno@users.noreply.github.com>
2023-08-10 20:39:25 +0000

    commit change, added icons🔵

--- enhanced_dev_Disk-Arrays - Copy (2).py
+++ enhanced_dev_Disk-Arrays - Copy (2).py
@@ -28,7 +28,7 @@ def main():
 		return crt.Screen.WaitForStrings ([ "#", ">", ":", "OK."])
 
 	def get_array_name(array_name):
-		array_name=crt.Dialog.Prompt("-->Disk Array Name :", "► Enter Array Name. ", "Psh commit from--IvanC", False)
+		array_name=crt.Dialog.Prompt("-->Disk Array Name :", "► Enter Array Name. ", "🔥🎯📝Psh commit from--IvanC🔥🎯📝", False)
 		return array_name
 	def return_controller_select(id_controller):
 		id_controller= crt.Dialog.Prompt("■ Select Controller (A|B): ", "► Select Controller from menu: ", "A|B", False)
